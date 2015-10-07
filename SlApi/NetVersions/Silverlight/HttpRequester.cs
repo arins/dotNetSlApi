cf54a6b66e56ec74ce7f3f001da106e41d24bd15
Arin Sinabian <arin.sinabian@gmail.com>
2015-10-08 01:08:34 +0200

    icon and .net 45

---
+++ SlApi/NetVersions/Silverlight/HttpRequester.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Threading.Tasks;
+using SlApi.Core;
+
+
+namespace SlApi
+{
+
+    internal class HttpRequester : IHttpRequester
+    {
+
+        /// <summary>
+        /// The timeout until abort in milliseconds
+        /// </summary>
+        public int Timeout { get; set; }
+
+        public bool GzipEnabled
+        {
+            get
+            {
+                throw new NotSupportedException("This is not supported in .net silverlight 5.0");
+            }
+            set
+            {
+                throw new NotSupportedException("This is not supported in .net silverlight 5.0");
+            }
+        }
+
+        internal HttpRequester()
+        {
+            Timeout = 10000;
+            GzipEnabled = false;
+        }
+
+        /// <summary>
+        /// Gets the response string async from the url
+        /// </summary>
+        /// <param name="url">url to request from</param>
+        /// <returns>return a string in the encoding specified</returns>
+        /// <exception cref="RequestException">Throw this exception if any error occurs</exception>
+        public async Task<string> GetResponseAsync(Uri url)
+        {
+            try
+            {
+                var client = WebRequest.CreateHttp(url);
+                
+                client.Method = "GET";
+                
+                var webresponse = client.GetResponseAsync();
+                await webresponse.WaitWithTimeoutAsync(Timeout);
+                var stream = webresponse.Result.GetResponseStream();
+                if (stream == null)
+                {
+                    throw new RequestException("stream from response is null!");
+                }
+                using (var streamReader = new StreamReader(stream))
+                {
+                    return streamReader.ReadToEnd();
+                }
+
+            }
+            catch (Exception e)
+            {
+                throw new RequestException("Request failed check inner exception", e);
+            }
+            
+        }
+
+
+        /// <summary>
+        /// Gets the response string from the url
+        /// </summary>
+        /// <param name="url">url to request from</param>
+        /// <returns>return a string in the encoding specified</returns>
+        /// <exception cref="RequestException">Throw this exception if any error occurs</exception>
+        public string GetResponse(Uri url)
+        {
+            try
+            {
+
+                var client = WebRequest.CreateHttp(url);
+                client.Method = "GET";
+                var webresponse = client.GetResponseAsync();
+                webresponse.WaitWithTimeout(Timeout);
+                var stream = webresponse.Result.GetResponseStream();
+                if (stream == null)
+                {
+                    throw new RequestException("stream from response is null!");
+                }
+                using (var streamReader = new StreamReader(stream))
+                {
+                    return streamReader.ReadToEnd();
+                }
+
+            }
+            catch (Exception e)
+            {
+                throw new RequestException("Request failed check inner exception", e);
+            }
+        }
+
+    }
+}
