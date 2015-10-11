ec91908ce3fd75bfb8665a97880fdbccba36d408
Arin Sinabian <arin.sinabian@gmail.com>
2015-10-11 16:44:42 +0200

    moved more testdata to files

--- SlApi/SlApi.Tests/NearbyStopsClientTest.cs
+++ SlApi/SlApi.Tests/NearbyStopsClientTest.cs
@@ -8,7 +8,8 @@ using SlApi.Models.NearbyStations.Request;
 namespace SlApi.Tests
 {
     [TestClass]
-    public class NearbyStopsClientTest
+    [DeploymentItem("TestData\\NearbyStopsClient\\success.json", "TestData\\NearbyStopsClient")]
+    public class NearbyStopsClientTest : SlApiTest
     {
        
 
@@ -110,31 +111,13 @@ namespace SlApi.Tests
 
         public string GetNearbyStationsErrorDataForTest()
         {
-            return
-                "{\"LocationList\":{  \"noNamespaceSchemaLocation\":\"\",  \"errorCode\":\"R0002\",  \"errorText\":\"Ogiltiga eller saknade parametrar\"  }}";
+            return GetSampleResponse("NearbyStopsClient\\error.json");
         }
 
 
         public string GetNearbyStationDataForTest()
         {
-            return
-                "{\"LocationList\":{  \"noNamespaceSchemaLocation\":\"hafasRestLocation.xsd\",  \"StopLocation\":[{ " +
-                "   \"idx\":\"1\",    \"name\":\"Trappgränd (Solna)\",    \"id\":\"300103467\",    \"lat\":\"59.365571\", " +
-                "   \"lon\":\"17.998435\",    \"dist\":\"17\"    },{    \"idx\":\"2\",    \"name\":\"Stråket (Solna)\",  " +
-                "  \"id\":\"300103460\",    \"lat\":\"59.365004\",    \"lon\":\"17.995496\",    \"dist\":\"163\"    },{ " +
-                "   \"idx\":\"3\",    \"name\":\"Vasalund (Solna)\",    \"id\":\"300103453\",    \"lat\":\"59.366299\", " +
-                "   \"lon\":\"18.000898\",    \"dist\":\"175\"    },{    \"idx\":\"4\",    \"name\":\"Fotbollsstadion (Solna)\"," +
-                "    \"id\":\"300103466\",    \"lat\":\"59.363234\",    \"lon\":\"17.997581\",    \"dist\":\"264\" " +
-                "   },{    \"idx\":\"5\",    \"name\":\"Råsunda (Solna)\",    \"id\":\"300103469\",   " +
-                " \"lat\":\"59.365337\",    \"lon\":\"17.993195\",    \"dist\":\"281\"    },{    \"idx\":\"6\",   " +
-                " \"name\":\"Östervägen (Solna)\",    \"id\":\"300103448\",    \"lat\":\"59.367665\",   " +
-                " \"lon\":\"17.995235\",    \"dist\":\"282\"    },{    \"idx\":\"7\",    \"name\":\"Solna centrum norra (Solna)\", " +
-                "   \"id\":\"300103472\",    \"lat\":\"59.361696\",    \"lon\":\"17.999775\",    \"dist\":\"443\"  " +
-                "  },{    \"idx\":\"8\",    \"name\":\"Frösundaleden (Solna)\",    \"id\":\"300103464\",  " +
-                "  \"lat\":\"59.361678\",    \"lon\":\"18.000476\",    \"dist\":\"455\"    },{    \"idx\":\"9\",  " +
-                "  \"name\":\"Pyramidvägen (Solna)\",    \"id\":\"300103459\",    \"lat\":\"59.366685\", " +
-                "   \"lon\":\"18.006130\",    \"dist\":\"469\"    },{    \"idx\":\"10\",    \"name\":\"Dalvägen (Solna)\", " +
-                "   \"id\":\"300103454\",    \"lat\":\"59.365346\",    \"lon\":\"18.006705\",    \"dist\":\"486\"    }]  }}";
+            return GetSampleResponse("NearbyStopsClient\\success.json");
         }
     }
 }
