6bb4e2511999e474eaea469c4e003b1b17681208
Arin Sinabian <arin.sinabian@gmail.com>
2015-10-11 22:25:11 +0200

    cleaned up stuff

---
+++ SlApi/SlApi.Tests/SlApiCallbackTest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SlApi.Tests
+{
+    public class SlApiCallbackTest : SlApiTest
+    {
+        public void Test()
+        {
+            
+        }
+    }
+}
