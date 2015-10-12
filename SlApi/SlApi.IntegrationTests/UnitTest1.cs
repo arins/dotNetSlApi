52aaabf9ee36094e6a769e54e6acc8e0ac6dac7c
Arin Sinabian <arin.sinabian@gmail.com>
2015-10-12 16:50:58 +0200

    test

--- SlApi/SlApi.IntegrationTests/UnitTest1.cs
+++ SlApi/SlApi.IntegrationTests/UnitTest1.cs
@@ -12,21 +12,37 @@ namespace SlApi.IntegrationTests
         {
             Trace.WriteLine(Environment.UserDomainName);
             var test = Environment.GetEnvironmentVariable("PlaceSearchClientApiToken",EnvironmentVariableTarget.Machine);
+            if (test != null)
+            {
+                Trace.WriteLine(" EnvironmentVariableTarget.Machine");
+            }
             if (test == null)
             {
                 test = Environment.GetEnvironmentVariable("PlaceSearchClientApiToken", EnvironmentVariableTarget.User);
+                if (test != null)
+                {
+                    Trace.WriteLine(" EnvironmentVariableTarget.User");
+                }
             }
             if (test == null)
             {
                 test = Environment.GetEnvironmentVariable("PlaceSearchClientApiToken", EnvironmentVariableTarget.Process);
+                if (test != null)
+                {
+                    Trace.WriteLine(" EnvironmentVariableTarget.Process");
+                }
             }
             if (test == null)
             {
                 test = Environment.GetEnvironmentVariable("PlaceSearchClientApiToken");
+                if (test != null)
+                {
+                    Trace.WriteLine(" EnvironmentVariableTarget.User");
+                }
             }
             if (test != null)
             {
-                Trace.WriteLine(test);
+                Trace.WriteLine("PlaceSearchClientApiToken: " + test);
             }
             else
             {
