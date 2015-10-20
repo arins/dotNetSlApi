e30be3828c7f6a3cdb96370b8850ebfd9a6a27fb
Arin Sinabian <arin.sinabian@gmail.com>
2015-10-21 00:21:29 +0200

    build error

---
+++ SlApi/SlApi.IntegrationTests46/TrafficDeviationInformationClientIntegrationTest.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SlApi.Models.RealtimeInformation.Request;
+using SlApi.Models.TrafficDeviationInformation.Request;
+
+namespace SlApi.IntegrationTests
+{
+    [TestClass]
+    public class TrafficDeviationInformationClientIntegrationTest
+    {
+        [TestMethod]
+        public void GetTrafficDeviationInformationIntegrationFailTest()
+        {
+            var token = EnvironmentHelper.GetEnvironmentVariable("TrafficDeviationInformationClientApiToken");
+            var client = new TrafficDeviationInformationClient(token)
+            {
+                GzipEnabled = true,
+
+            };
+            var result = client.GetTrafficDeviationInformation(new TrafficDeviationInformationRequest());
+            if (result.ResponseData.Any())
+            {
+                Assert.IsTrue(result.ResponseData.All(r => r.Created.Date.Year > 2013));
+            }
+
+        }
+    }
+}
---
+++ SlApi/SlApi.IntegrationTests46/TrafficInformationClientIntegrationTest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SlApi.Models;
+
+namespace SlApi.IntegrationTests
+{
+    [TestClass]
+    public class TrafficInformationClientIntegrationTest
+    {
+        [TestMethod]
+        public void TrafficInformationClientTest()
+        {
+            var token = EnvironmentHelper.GetEnvironmentVariable("TrafficInformationClientApiToken");
+            var t = new TrafficInformationClient(token);
+            var result = t.GetTrafficInformation();
+            Assert.IsTrue(result.StatusCode == StatusCode.Ok);
+        }
+    }
+}
